29173f13b0deca194ac658efe6a51098dd36fe3b
Mitch <mbperson@ualr.edu>
2025-05-05 19:13:19 +0000

    cycle demo slick style

---
+++ cycle-demo/assets/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnHover: true,
+      arrows: true
+    });
+  });
+  
